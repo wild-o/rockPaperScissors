9535cac7d1e4087bedf27378341e209bcb934b95
Albert <alarias1@outlook.com>
2021-04-19 18:06:02 -0500

    playRound function added

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,7 +1,12 @@
 
 function computerPlay(){
-    const textArray = ['rock', 'paper', 'scissors'];
+    const textArray = ['Rock', 'Paper', 'Scissors'];
     const randomNumber = Math.floor(Math.random() * textArray.length);
 
     console.log(textArray[randomNumber]);
+}
+
+function playRound(playerSelection, computerSelections){
+    const selection = window.prompt("Make your choice: ");
+    console.log(selection);
 }
